527d35ee933a1124bafe5aa0c7850a51a3d93dd1
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2026-06-27 09:17:59 +0500

    Update onboarding.tsx

--- src/app/onboarding.tsx
+++ src/app/onboarding.tsx
@@ -61,7 +61,7 @@ export default function Onboarding() {
 
       <TouchableOpacity
         style={styles.button}
-        onPress={() => router.push("/sign in")}
+        onPress={() => router.push("/sign-in")}
       >
         <Text style={styles.buttonText}>→</Text>
       </TouchableOpacity>
